e842ffb216b78359580e34735107067eca1a486a
Joe Masilotti <joseph.masilotti@gmail.com>
2013-10-14 10:12:40 -0400

    Register for app messages.

--- src/PebCiti-Pebble.c
+++ src/PebCiti-Pebble.c
@@ -9,6 +9,8 @@ PBL_APP_INFO(MY_UUID, "PebCiti", "Masilotti.com", 1, 0, DEFAULT_MENU_ICON, APP_I
 static struct PebCitiData
 {
   Window window;
+  AppSync sync;
+  uint8_t sync_buffer[32];
 } s_data;
 
 static void peb_citi_init(AppContextRef context)
@@ -16,13 +18,21 @@ static void peb_citi_init(AppContextRef context)
   Window *window = &s_data.window;
   window_init(window, "PebCiti");
 
+  app_sync_init(&s_data.sync, s_data.sync_buffer, sizeof(s_data.sync_buffer), NULL, 0, NULL, NULL, NULL);
+
   window_stack_push(window, true);
+}
+
+static void peb_citi_deinit(AppContextRef context)
+{
+  app_sync_deinit(&s_data.sync);
 }
 
 void pbl_main(void *params)
 {
   PebbleAppHandlers handlers = {
-    .init_handler = &peb_citi_init
+    .init_handler = &peb_citi_init,
+    .deinit_handler = &peb_citi_deinit
   };
   app_event_loop(params, &handlers);
 }
