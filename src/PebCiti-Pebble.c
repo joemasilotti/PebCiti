e4d5048f9e63ced936aacbd3a08cf0ac5511c7bb
Joe Masilotti <joseph.masilotti@gmail.com>
2013-10-24 23:21:17 -0400

    Upgrade to SDK v2.0

--- src/PebCiti-Pebble.c
+++ src/PebCiti-Pebble.c
@@ -2,43 +2,53 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 
-#define MY_UUID { 0xF6, 0xBB, 0x82, 0xD0, 0xB5, 0xBF, 0x4E, 0xC7, 0xA9, 0x7A, 0x40, 0x5D, 0x3A, 0x35, 0x04, 0x44 }
+static Window *window;
+static TextLayer *text_layer;
+static AppSync sync;
+static uint8_t sync_buffer[32];
 
-PBL_APP_INFO(MY_UUID, "PebCiti", "Masilotti.com", 1, 0, DEFAULT_MENU_ICON, APP_INFO_STANDARD_APP);
-
-static struct PebCitiData
-{
-    Window window;
-    TextLayer text_layer;
-    AppSync sync;
-    uint8_t sync_buffer[32];
-} s_data;
-
-enum {
+enum PebCitiKey {
     PEB_CITI_TEXT_KEY = 0x1
 };
 
 static void sync_tuple_changed_callback(const uint32_t key, const Tuple *new_tuple, const Tuple *old_tuple, void *context)
 {
-    text_layer_set_text(&s_data.text_layer, new_tuple->value->cstring);
+    text_layer_set_text(text_layer, new_tuple->value->cstring);
 }
 
-static void peb_citi_init(AppContextRef context)
+static void window_load(Window *window)
 {
-    Window *window = &s_data.window;
-    window_init(window, "PebCiti");
+    Layer *window_layer = window_get_root_layer(window);
 
-    text_layer_init(&s_data.text_layer, GRect(0, 100, 144, 68));
-    layer_add_child(&window->layer, &s_data.text_layer.layer);
-    text_layer_set_text_alignment(&s_data.text_layer, GTextAlignmentCenter);
+    text_layer = text_layer_create(GRect(0, 100, 144, 68));
+    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+    layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void window_unload(Window *window)
+{
+    text_layer_destroy(text_layer);
+}
+
+static void init()
+{
+    window = window_create();
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload
+    });
 
     Tuplet initial_values[] = {
         TupletCString(PEB_CITI_TEXT_KEY, "<empty>")
     };
 
-    app_sync_init(&s_data.sync,
-                  s_data.sync_buffer,
-                  sizeof(s_data.sync_buffer),
+    const int inbound_size = 64;
+    const int outbound_size = 16;
+    app_message_open(inbound_size, outbound_size);
+
+    app_sync_init(&sync,
+                  sync_buffer,
+                  sizeof(sync_buffer),
                   initial_values,
                   ARRAY_LENGTH(initial_values),
                   sync_tuple_changed_callback,
@@ -48,22 +58,15 @@ static void peb_citi_init(AppContextRef context)
     window_stack_push(window, true);
 }
 
-static void peb_citi_deinit(AppContextRef context)
+static void deinit()
 {
-    app_sync_deinit(&s_data.sync);
+    app_sync_deinit(&sync);
+    window_destroy(window);
 }
 
-void pbl_main(void *params)
+int main(void)
 {
-    PebbleAppHandlers handlers = {
-        .init_handler = &peb_citi_init,
-        .deinit_handler = &peb_citi_deinit,
-        .messaging_info = {
-            .buffer_sizes = {
-                .inbound = 64,
-                .outbound = 16
-            }
-        }
-    };
-    app_event_loop(params, &handlers);
+    init();
+    app_event_loop();
+    deinit();
 }
