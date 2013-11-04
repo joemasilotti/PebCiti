481f2f351be4c12f42785301275e97bb3a277a2a
Joe Masilotti <joseph.masilotti@gmail.com>
2013-11-03 20:44:39 -0500

    Source ~/.zshrc in Xcode build script.

--- xcode/waf-xcode.sh
+++ xcode/waf-xcode.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Expecting arm toolchain + openocd binaries to be in $PATH after sourcing .bash_profile:
 source ~/.bash_profile
+source ~/.zshrc
 cd ..
 if [ -z $ACTION ]; then
     ACTION=build
