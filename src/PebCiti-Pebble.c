b2d974150cfe4472ca9c7fa56016b4314a4a7a27
Joe Masilotti <joseph.masilotti@gmail.com>
2013-11-12 22:51:53 -0500

    Drop inbound app message size to 124.

--- src/PebCiti-Pebble.c
+++ src/PebCiti-Pebble.c
@@ -68,7 +68,7 @@ static void init()
         TupletInteger(PEB_CITI_VIBRATE_KEY, (uint8_t) 0)
     };
 
-    const int inbound_size = 128;
+    const int inbound_size = 124;
     const int outbound_size = 16;
     app_message_open(inbound_size, outbound_size);
 
